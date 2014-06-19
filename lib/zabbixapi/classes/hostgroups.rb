f00950fbcc2e3d3ace48b5be1ec6a24b61629cf6
Daniel Campos <daniel.campos@bodybuilding.com>
2014-06-19 13:00:41 -0600

    Added get_hosts to be able to get the hosts from an specific hostgroup

--- lib/zabbixapi/classes/hostgroups.rb
+++ lib/zabbixapi/classes/hostgroups.rb
@@ -16,5 +16,21 @@ class ZabbixApi
     def key
       "groupid"
     end
+
+    def get_hosts(data)
+      log "[DEBUG] Call get_hosts with parametrs: #{data.inspect}"
+
+      @client.api_request(
+        :method => "#{method_name}.get",
+        :params => {
+          :filter => {
+            indentify.to_sym => data[indentify.to_sym]
+          },
+          :selectHosts => [:hostid,:name]
+          #:output => [:hostid]
+        }
+      )
+    end
+
   end
 end
