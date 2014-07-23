ccc70c207938c0c9d725b1d5e8617900dc3f9b7e
Daniel Campos <daniel.campos@bodybuilding.com>
2014-07-23 02:00:15 -0600

    Added get_pairs and hash_equals in order to overwrite the functions from basic_logic. Also added debug lines to the other functions.

--- lib/zabbixapi/classes/graphs.rb
+++ lib/zabbixapi/classes/graphs.rb
@@ -13,6 +13,30 @@ class ZabbixApi
       "name"
     end
 
+    # Check which keys from n exists in d,
+    # then return the matches in a hash.
+    def get_pairs(d, n)
+      matches = {}
+      n.keys.each {|key|
+        if d.has_key?(key)
+          matches["#{key}"] = d["#{key}"]
+        end
+      }
+      matches
+    end
+
+    # this is a different version from the one declared
+    # in basic_logic
+    def hash_equals?(a, b)
+      t = get_pairs(a, b)
+      if (b.size > t.size)
+        difference = b.to_a - t.to_a
+      else
+        difference = t.to_a - b.to_a
+      end
+      difference.size == 0
+    end
+
     def get_full_data(data)
       log "[DEBUG] Call get_full_data with parameters: #{data.inspect}"
 
@@ -72,15 +96,18 @@ class ZabbixApi
     end
 
     def get_items(data)
+      log "[DEBUG] Call get_items with parameters: #{data.inspect}"
       @client.api_request(:method => "graphitem.get", :params => { :graphids => [data], :output => "extend" } )
     end
 
     def create_or_update(data)
+      log "[DEBUG] Call create_or_update with parameters: #{data.inspect}"
       graphid = get_id(:name => data[:name], :templateid => data[:templateid])
       graphid ? _update(data.merge(:graphid => graphid)) : create(data)
     end
 
     def _update(data)
+      log "[DEBUG] Call _update with parameters: #{data.inspect}"
       data.delete(:name)
       update(data)
     end
